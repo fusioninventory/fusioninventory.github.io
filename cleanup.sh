0ae033b1b9bcb5b45c156e990592d27456656073
Gonéri Le Bouder <goneri@rulezlan.org>
2013-01-28 14:27:29 +0100

    quiet dos2unix

--- cleanup.sh
+++ cleanup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 find . -name '*.po' -exec sed -i 's, \+$,,' {} \;
-find . -name '*.po' -exec dos2unix {} \;
+find . -name '*.po' -exec dos2unix -q {} \;
 find -name '*.po' -exec msgcat --strict {} > /dev/null \;
